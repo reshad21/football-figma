f89f845666d2ff4f3e601c6a060aee1504dfb10d
reshad21 <miakala7@gmail.com>
2023-04-30 16:43:26 +0600

    polar slider card design

--- src/components/PolarChart.jsx
+++ src/components/PolarChart.jsx
@@ -1,17 +1,16 @@
 import {
+    ArcElement,
     Chart as ChartJS,
-    LineElement,
-    PointElement,
-    RadarController,
-    RadialLinearScale,
+    PolarAreaController,
+    RadialLinearScale
 } from 'chart.js';
 import React from 'react';
-import { Radar } from 'react-chartjs-2';
+import { PolarArea } from 'react-chartjs-2';
+import { Link } from 'react-router-dom';
 ChartJS.register(
-    RadarController,
-    RadialLinearScale,
-    PointElement,
-    LineElement,
+    PolarAreaController,
+    ArcElement,
+    RadialLinearScale
 )
 
 
@@ -19,58 +18,58 @@ const PolarChart = () => {
 
     const data = {
         labels: [
-            'Eating',
-            'Drinking',
-            'Sleeping',
-            'Designing',
-            'Coding',
-            'Cycling',
-            'Running'
+            'Red',
+            'Green',
+            'Yellow',
+            'Grey',
+            'Blue',
+            'Orange',
         ],
         datasets: [{
             label: 'My First Dataset',
-            data: [65, 59, 90, 81, 56, 55, 40],
-            fill: true,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgb(255, 99, 132)',
-            pointBackgroundColor: 'rgb(255, 99, 132)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(255, 99, 132)'
-        }, {
-            label: 'My Second Dataset',
-            data: [28, 48, 40, 19, 96, 27, 100],
-            fill: true,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgb(54, 162, 235)',
-            pointBackgroundColor: 'rgb(54, 162, 235)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(54, 162, 235)'
+            data: [11, 16, 7, 3, 14, 19],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(75, 192, 192)',
+                'rgb(255, 205, 86)',
+                'rgb(201, 203, 207)',
+                'rgb(54, 162, 235)',
+                'rgb(201, 205, 235)',
+            ]
         }]
     };
 
     const options = {
+        responsive: true,
         scales: {
             r: {
-                angleLines: {
-                    display: false
-                },
-                suggestedMin: 50,
-                suggestedMax: 100
+                pointLabels: {
+                    display: true,
+                    font: {
+                        size: 14
+                    },
+                    padding: 0
+                }
             }
         }
+
+
     };
 
 
 
 
     return (
-        <div>
-            <Radar
+        <div className='card bg-base-100 w-[385px] py-3 rounded-md'>
+            <div className="cardHeader pb-3 px-4 flex justify-evenly">
+                <Link className='btn w-[102px] h-[38px] rounded-full btn-outline'>Model</Link>
+                <Link className='btn w-[102px] h-[38px] rounded-full bg-[#14314E]'>Style</Link>
+                <Link className='btn w-[120px] h-[38px] rounded-full btn-outline'>Contribution</Link>
+            </div>
+            <PolarArea
                 data={data}
                 options={options}
-            ></Radar>
+            ></PolarArea>
         </div>
     );
 };
