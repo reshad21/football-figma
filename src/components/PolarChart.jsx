9236b6675b562b737a4ca0987aa7d1641a6530e4
reshad21 <miakala7@gmail.com>
2023-04-30 16:05:33 +0600

    rader chart added

--- src/components/PolarChart.jsx
+++ src/components/PolarChart.jsx
@@ -1,59 +1,76 @@
 import {
-    BarElement,
-    CategoryScale,
     Chart as ChartJS,
-    Legend,
-    LinearScale,
-    Tooltip
+    LineElement,
+    PointElement,
+    RadarController,
+    RadialLinearScale,
 } from 'chart.js';
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Radar } from 'react-chartjs-2';
 ChartJS.register(
-    BarElement,
-    CategoryScale,
-    LinearScale,
-    Tooltip,
-    Legend
+    RadarController,
+    RadialLinearScale,
+    PointElement,
+    LineElement,
 )
 
 
 const PolarChart = () => {
 
     const data = {
-        labels: ['monday', 'saturday', 'sunday', 'friday', 'thursday'],
-        datasets: [
-            {
-                label: 'level of thiccness',
-                data: [32, 45, 12, 76, 69],
-                backgroundColor: [
-                    'rgba(75,192,192,0.6)'
-                ],
-                borderWidth: 2,
-                borderColor: 'black'
-            },
-            {
-                label: 'level of thiccness',
-                data: [32, 45, 12, 76, 69],
-                backgroundColor: [
-                    'rgba(75,192,192,0.6)'
-                ],
-                borderWidth: 2,
-                borderColor: 'green'
-            }
-        ]
-    }
+        labels: [
+            'Eating',
+            'Drinking',
+            'Sleeping',
+            'Designing',
+            'Coding',
+            'Cycling',
+            'Running'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 90, 81, 56, 55, 40],
+            fill: true,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgb(255, 99, 132)',
+            pointBackgroundColor: 'rgb(255, 99, 132)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(255, 99, 132)'
+        }, {
+            label: 'My Second Dataset',
+            data: [28, 48, 40, 19, 96, 27, 100],
+            fill: true,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgb(54, 162, 235)',
+            pointBackgroundColor: 'rgb(54, 162, 235)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(54, 162, 235)'
+        }]
+    };
 
-    const options = {}
+    const options = {
+        scales: {
+            r: {
+                angleLines: {
+                    display: false
+                },
+                suggestedMin: 50,
+                suggestedMax: 100
+            }
+        }
+    };
 
 
 
 
     return (
         <div>
-            <Bar
+            <Radar
                 data={data}
                 options={options}
-            ></Bar>
+            ></Radar>
         </div>
     );
 };
